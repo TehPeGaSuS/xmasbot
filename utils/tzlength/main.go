95b01560801e717b9aab62bdf0de8672ffe82d43
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2025-12-20 20:11:57 +0100

    Add utility to print lengths of zone representations
    This utility prints lengths of zones string representation, useful for checking if a zone exceeds IRC limits.

---
+++ utils/tzlength/main.go
@@ -0,0 +1,32 @@
+// This utility prints lengths of zones string representation. Useful for seeing if some zone exceeds irc limit
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"text/tabwriter"
+
+	"github.com/TehPeGaSuS/xmasbot/nyb"
+)
+
+func main() {
+	var zones nyb.TZS
+	err := json.Unmarshal(nyb.Zones, &zones)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Sort(sort.Reverse(zones))
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', tabwriter.AlignRight|tabwriter.Debug)
+
+	for _, k := range zones {
+		tmp := len(k.String())
+		if tmp > 0 {
+			fmt.Fprintf(w, "%v\t%d\t%d\t\n", k.Offset, tmp, tmp-396)
+		}
+	}
+	w.Flush()
+}
