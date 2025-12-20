f696fb02bbfb11caa941e34969552b54f7d9b31c
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2025-12-20 20:01:22 +0100

    Implement sorting for time zone JSON data
    This program sorts time zone data from a JSON source and saves it to a file.

---
+++ utils/sorter/main.go
@@ -0,0 +1,84 @@
+// Util for sorting the TZ json
+// To make it neat and nice after manual edits
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"sort"
+
+	nyb "github.com/TehPeGaSuS/xmasbot/nyb"
+)
+
+func main() {
+	var v TZS
+	err := json.Unmarshal(nyb.Zones, &v)
+	if err != nil {
+		panic(err)
+	}
+	sort.Sort(v)
+	for i := range v {
+		sort.Sort(v[i])
+		for j := range v[i].Countries {
+			sort.Sort(v[i].Countries[j].Cities)
+		}
+	}
+	data, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.WriteFile("tz.json", data, 0644)
+}
+
+// Cities ...
+type Cities []string
+
+// TZ holds infor for Time Zone
+// TZ holds info for Time Zone
+type TZ struct {
+	Countries []struct {
+		Name   string `json:"name"`
+		Cities Cities `json:"cities"`
+	} `json:"countries"`
+	Offset float64 `json:"offset"`
+}
+
+// TZS is a slice of TZ
+type TZS []TZ
+
+func (t Cities) Len() int {
+	return len(t)
+}
+
+func (t Cities) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t Cities) Less(i, j int) bool {
+	return t[i] < t[j]
+}
+
+func (t TZ) Len() int {
+	return len(t.Countries)
+}
+
+func (t TZ) Swap(i, j int) {
+	t.Countries[i], t.Countries[j] = t.Countries[j], t.Countries[i]
+}
+
+func (t TZ) Less(i, j int) bool {
+	return t.Countries[i].Name < t.Countries[j].Name
+}
+
+func (t TZS) Len() int {
+	return len(t)
+}
+
+func (t TZS) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t TZS) Less(i, j int) bool {
+	return t[i].Offset < t[j].Offset
+}
