6b1d48260a175c0ecc35c61339c41e15799018de
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2025-12-20 20:28:18 +0100

    Implement TestZones function for zone validation
    Add tests to validate zone, country, and city names

---
+++ nyb/tz_test.go
@@ -0,0 +1,26 @@
+package nyb
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestZones(t *testing.T) {
+	var zones TZS
+	err := json.Unmarshal(Zones, &zones)
+	if err != nil {
+		t.Errorf("Corrupted zone data: %s", err)
+	}
+	for _, zone := range zones {
+		for _, country := range zone.Countries {
+			if country.Name == "" {
+				t.Errorf("Empty country name in zone %v", zone.Offset)
+			}
+			for _, city := range country.Cities {
+				if city == "" {
+					t.Errorf("Empty city name in %s in zone %v", country.Name, zone.Offset)
+				}
+			}
+		}
+	}
+}
