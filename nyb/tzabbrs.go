c5502a27a496b339758cfeb96c1844b4a45b2e99
TehPeGaSuS <25697531+TehPeGaSuS@users.noreply.github.com>
2025-12-20 20:29:08 +0100

    Add timezone abbreviation parsing functionality

---
+++ nyb/tzabbrs.go
@@ -0,0 +1,183 @@
+package nyb
+
+import (
+	"bytes"
+	_ "embed"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// source: https://timezonedb.com/download
+//
+//go:embed time_zone.csv
+var tzCSV []byte
+
+var tzAbbrs = parseZoneInfo(tzCSV, target)
+
+func parseZoneInfo(data []byte, target time.Time) (abbrs map[string]int) {
+	type zoneInfo struct {
+		abbr   string
+		offset int
+	}
+	type zoneMap map[string]zoneInfo
+	zones := make(zoneMap)
+
+	entries := bytes.Split(data, []byte{'\n'})
+	var zoneName string
+	var country2iso string
+	var abbr string
+	var unixTime int64
+	var offsetSec int
+	var dst bool
+	for _, entry := range entries {
+		entry = bytes.TrimSpace(entry)
+		if len(entry) == 0 {
+			break
+		}
+		entry = bytes.ReplaceAll(entry, []byte{','}, []byte{' '})
+
+		_, err := fmt.Sscanf(
+			string(entry),
+			"%s %s %s %d %d %t",
+			&zoneName,
+			&country2iso,
+			&abbr,
+			&unixTime,
+			&offsetSec,
+			&dst,
+		)
+		if errors.Is(err, io.EOF) {
+			continue
+		}
+		if err != nil {
+			log.Fatal("zone abbr parse", err)
+		}
+
+		abbr = strings.ToUpper(abbr)
+		if unixTime <= target.Unix() {
+			zones[zoneName] = zoneInfo{
+				abbr:   abbr,
+				offset: offsetSec,
+			}
+		}
+	}
+
+	counter := make(map[string]map[int]int)
+	for _, zone := range zones {
+		if counter[zone.abbr] == nil {
+			counter[zone.abbr] = make(map[int]int)
+		}
+		counter[zone.abbr][zone.offset]++
+	}
+
+	abbrs = make(map[string]int)
+	var max int
+	for abbr, offsets := range counter {
+		max = 0
+		for offset, count := range offsets {
+			if count > max {
+				max = count
+				abbrs[abbr] = offset
+			}
+		}
+	}
+	abbrs["UTC"] = 0
+	abbrs["Z"] = 0
+	milneg := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M"}
+	milpos := []string{"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y"}
+	for i := 1; i <= 12; i++ {
+		abbrs[milneg[i-1]] = -i * 3600
+		abbrs[milpos[i-1]] = i * 3600
+	}
+	tzCSV = nil
+	return abbrs
+}
+
+var ErrTZParser = errors.New("parser error")
+var ErrTZOutOfRange = errors.New("value out of range")
+
+func parseUTC(in string) (int, error) {
+	var offset int64
+	formats := []string{
+		"UTC+%d:%d",
+		"UTC-%d:%d",
+		"GMT+%d:%d",
+		"GMT-%d:%d",
+	}
+	for i := range formats {
+		var hours int64
+		var minutes int64
+		_, err := fmt.Sscanf(in, formats[i], &hours, &minutes)
+		if err == nil {
+			if hours < 0 || minutes < 0 || minutes > 59 {
+				return 0, fmt.Errorf("%w: invalid format", ErrTZParser)
+			}
+			if hours >= math.MaxInt64/int64(time.Second)/3600 {
+				return 0, fmt.Errorf("%w: %w", ErrTZParser, ErrTZOutOfRange)
+			}
+			if i%2 == 0 {
+				offset += hours * 3600
+				offset += minutes * 60
+				if offset < 0 {
+					return 0, fmt.Errorf("%w: overflow", ErrTZParser)
+				}
+			} else {
+				offset -= hours * 3600
+				offset -= minutes * 60
+				if offset > 0 {
+					return 0, fmt.Errorf("%w: underflow", ErrTZParser)
+				}
+			}
+			if i > 1 {
+				offset = -offset
+			}
+			return int(offset), nil
+		}
+		if errors.Is(err, strconv.ErrRange) {
+			return 0, fmt.Errorf("%w: %w", ErrTZParser, ErrTZOutOfRange)
+		}
+	}
+	formatsShort := []string{
+		"UTC+%d",
+		"UTC-%d",
+		"GMT+%d",
+		"GMT-%d",
+	}
+	for i := range formatsShort {
+		var hours int64
+		_, err := fmt.Sscanf(in, formatsShort[i], &hours)
+		if err == nil {
+			if hours < 0 {
+				return 0, fmt.Errorf("%w: invalid format", ErrTZParser)
+			}
+			if hours >= math.MaxInt64/int64(time.Second)/3600 {
+				return 0, fmt.Errorf("%w: %w", ErrTZParser, ErrTZOutOfRange)
+			}
+			if i%2 == 0 {
+				offset += hours * 3600
+				if offset < 0 {
+					return 0, fmt.Errorf("%w: overflow", ErrTZParser)
+				}
+			} else {
+				offset -= hours * 3600
+				if offset > 0 {
+					return 0, fmt.Errorf("%w: underflow", ErrTZParser)
+				}
+			}
+			if i > 1 {
+				offset = -offset
+			}
+			return int(offset), nil
+		}
+		if errors.Is(err, strconv.ErrRange) {
+			return 0, fmt.Errorf("%w: %w", ErrTZParser, ErrTZOutOfRange)
+		}
+	}
+	return 0, fmt.Errorf("zone not found")
+}
